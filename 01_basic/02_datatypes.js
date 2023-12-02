a06035132a1121540655e1798fe2e67f8dd6b49b
samreen246 <145689999+samreen246@users.noreply.github.com>
2023-12-02 07:18:20 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) -> give error because we are using nodeJs , not browser
+
+console.log(3+3); console.log("samreen") // without semi-colon it will give error, it is bad practice ,instead of doing this ,write it in next line because doing this is making our code readibilty bad
+//code readibility is priority
+
+let name = "samreen" 
+let age = 20 
+let isloggedIn = false 
+
+//--primitive data type--
+//string =>""(most prefer) or ''
+//number
+//boolean => true or false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "samreen") // it is string
+console.log(typeof null) // it is object
+console.log(typeof undefined) //it is a datatype
